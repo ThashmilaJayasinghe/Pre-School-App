a96ee7217a3404f0111770a16f92d4ded34b9dc0
Suboda Dabarera <SubodaDabarera99@gmail.com>
2023-03-24 04:10:46 +0530

    navbar added

---
+++ src/components/navbar/BottomNavbar.js
@@ -0,0 +1,66 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+import Octicons from 'react-native-vector-icons/Octicons';
+import Feather from 'react-native-vector-icons/Feather';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Color } from '../../GlobalStyles';
+
+const BottomNavbar = () => {
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        // paddingVertical: 13,
+        height: 45,
+        width: '100%',
+        borderTopEndRadius: 14,
+        borderTopStartRadius: 14,
+        backgroundColor: Color.mintcream,
+        marginHorizontal: 2,
+        justifyContent: 'center',
+
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 20,
+        }}>
+        <TouchableOpacity>
+          <Feather
+            name="home"
+            style={{
+              fontSize: 26,
+              fontWeight: '100',
+              color: '#C9C5C4'
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons
+            name="ios-heart-outline"
+            style={{
+              fontSize: 29,
+              fontWeight: '700',
+              color: '#C9C5C4'
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Feather
+            name="user"
+            style={{
+              fontSize: 27,
+              color: '#C9C5C4'
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default BottomNavbar

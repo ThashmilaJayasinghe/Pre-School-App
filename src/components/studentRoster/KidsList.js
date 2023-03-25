4fb2034230085f2e46ee13c3628d7f18e5a03083
Ramath <ramathmanjitha@gmail.com>
2023-03-25 11:08:29 +0530

    kid list

--- src/components/studentRoster/KidsList.js
+++ src/components/studentRoster/KidsList.js
@@ -3,30 +3,25 @@ import {
   Text,
   ScrollView,
   TextInput,
-  ImageBackground,
-  Button,
   TouchableOpacity,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import KidCard from './KidCard';
-import {Color, Border, FontFamily, FontSize} from '../../GlobalStyles';
+import {Color} from '../../GlobalStyles';
 import SaveKid from './SaveKid';
-import Modal from 'react-native-modal';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import DeleteKid from './Deleltekid';
-import {Alert} from 'react-native/Libraries/Alert/Alert';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 const studentList = [
   {id: '01', name: 'Kamal', class: 'Class A'},
   {id: '02', name: 'Nimal', class: 'Class B'},
-  // {id: '03', name: 'Sunimal', class: 'Class D'},
-  // {id: '04', name: 'Bimal', class: 'Class A'},
-  // {id: '05', name: 'John', class: 'Class E'},
-  // {id: '06', name: 'Mark', class: 'Class D'},
-  // {id: '07', name: 'Sunimal', class: 'Class D'},
-  // {id: '08', name: 'Bimal', class: 'Class A'},
-  // {id: '09', name: 'John', class: 'Class E'},
-  // {id: '10', name: 'Mark', class: 'Class D'},
+  {id: '03', name: 'Sunimal', class: 'Class D'},
+  {id: '04', name: 'Bimal', class: 'Class A'},
+  {id: '05', name: 'John', class: 'Class E'},
+  {id: '06', name: 'Mark', class: 'Class D'},
+  {id: '07', name: 'Sunimal', class: 'Class D'},
+  {id: '08', name: 'Bimal', class: 'Class A'},
+  {id: '09', name: 'John', class: 'Class E'},
+  {id: '10', name: 'Mark', class: 'Class D'},
 ];
 
 const KidsList = () => {
@@ -41,6 +36,8 @@ const KidsList = () => {
   useEffect(() => {
     fetchKids();
   }, [searchQuery]);
+
+  // useEffect(()=>{},[])
 
   const fetchKids = () => {
     if (searchQuery.trim()) {
@@ -79,12 +76,14 @@ const KidsList = () => {
         style={{
           backgroundColor: '#FBF8EB',
         }}>
-        {/* search bar */}
         <Text>Kid List</Text>
 
+        {/* search bar */}
         <View
           style={{
             alignItems: 'center',
+            marginVertical: 15,
+            top: 15,
           }}>
           <TextInput
             onChangeText={setSearchQuery}
@@ -101,13 +100,26 @@ const KidsList = () => {
               // height: 10,
               shadowRadius: 6,
               borderRadius: 6,
-              elevation: 6,
+              elevation: 0,
               shadowOpacity: 1,
-              height: 50,
+              height: 40,
               width: '90%',
-              paddingVertical: 10,
-              paddingHorizontal: 20,
+              paddingLeft: 10,
+              paddingRight: 40,
+              paddingHorizontal: 10,
+              color: '#626262',
             }}
+            placeholder="Search ..."
+          />
+          <FeatherIcon
+            name="search"
+            size={20}
+            style={{
+              top: 10,
+              right: 30,
+              position: 'absolute',
+            }}
+            color="#F47B0B"
           />
         </View>
         <View
@@ -116,8 +128,8 @@ const KidsList = () => {
             flexDirection: 'row',
             justifyContent: 'flex-end',
             alignItems: 'center',
-            marginRight: '8%',
-            marginTop: '5%',
+            marginRight: '5%',
+            marginTop: '7%',
             marginBottom: '5%',
           }}>
           <TouchableOpacity
@@ -142,159 +154,24 @@ const KidsList = () => {
               New Kid ?
             </Text>
           </TouchableOpacity>
-          {/* <Button title="New Kid ? " borderRadius="50" /> */}
         </View>
 
         <ScrollView>
           {state.map((student, idx) => (
-            <KidCard key={student.id} student={student} onDelete={onDelete} />
+            <KidCard
+              key={student.id}
+              student={student}
+              onDelete={onDelete}
+              kids={studentList}
+            />
           ))}
         </ScrollView>
       </View>
-
-      {/* //Modal */}
-
-      <Modal
-        isVisible={isModalVisible}
-        onBackdropPress={() => setModalVisible(false)}
-        style={{alignItems: 'center'}}>
-        <View
-          style={{
-            flex: 0,
-            backgroundColor: 'white',
-            width: '90%',
-            height: '80%',
-            borderRadius: 20,
-            paddingBottom: 20,
-          }}>
-          <TouchableOpacity
-            style={{marginTop: 13, marginRight: 13, alignItems: 'flex-end'}}
-            onPress={toggleModal}>
-            <AntDesign name="closesquare" size={27} color="#F47B0B" />
-          </TouchableOpacity>
-
-          <View style={{alignItems: 'center', marginVertical: 10}}>
-            <Text style={{fontSize: 17, fontWeight: 500}}>Save New Kid</Text>
-          </View>
-          {/* <KidPhoto /> */}
-          <ScrollView>
-            <View style={{margin: 20, maxHeight: 300}}>
-              <Text
-                style={{
-                  fontFamily: 'Alatsi',
-                  fontWeight: 400,
-                  fontSize: 14,
-                  lineHeight: 15,
-                  marginBottom: 10,
-                  color: '#616161',
-                }}>
-                Kid's ID
-              </Text>
-
-              <TextInput
-                onChangeText={setKidId}
-                style={{
-                  borderWidth: 1,
-                  borderColor: '#D6D6D6',
-                  borderRadius: 10,
-                  opacity: 10,
-                  padding: 5,
-                  backgroundColor: '#e8e9eb',
-                  fontSize: 14,
-                  color: '#8d8d8f',
-                }}
-                placeholder={`Add kid's ID here ...`}
-                placeholderTextColor={'#a7a7a8'}
-                underlineColorAndroid={'transparent'}
-              />
-            </View>
-
-            <View style={{margin: 20, maxHeight: 300}}>
-              <Text
-                style={{
-                  fontFamily: 'Alatsi',
-                  fontWeight: 400,
-                  fontSize: 14,
-                  lineHeight: 15,
-                  marginBottom: 10,
-                  color: '#616161',
-                }}>
-                Name
-              </Text>
-
-              <TextInput
-                onChangeText={setName}
-                style={{
-                  borderWidth: 1,
-                  borderColor: '#D6D6D6',
-                  borderRadius: 10,
-                  opacity: 10,
-                  padding: 5,
-                  backgroundColor: '#e8e9eb',
-                  fontSize: 14,
-                  color: '#8d8d8f',
-                }}
-                placeholder={`Add kid's name here ...`}
-                placeholderTextColor={'#a7a7a8'}
-                underlineColorAndroid={'transparent'}
-              />
-            </View>
-            <View style={{margin: 20, maxHeight: 300}}>
-              <Text
-                style={{
-                  fontFamily: 'Alatsi',
-                  fontWeight: 400,
-                  fontSize: 14,
-                  lineHeight: 15,
-                  marginBottom: 10,
-                  color: '#616161',
-                }}>
-                Class
-              </Text>
-
-              <TextInput
-                onChangeText={setKidClass}
-                style={{
-                  borderWidth: 1,
-                  borderColor: '#D6D6D6',
-                  borderRadius: 10,
-                  opacity: 10,
-                  padding: 5,
-                  backgroundColor: '#e8e9eb',
-                  fontSize: 14,
-                  color: '#8d8d8f',
-                }}
-                placeholder={`Add kid's class here ...`}
-                placeholderTextColor={'#a7a7a8'}
-                underlineColorAndroid={'transparent'}
-              />
-            </View>
-
-            <View style={{alignItems: 'center', marginBottom: 20}}>
-              <TouchableOpacity
-                onPress={addKid}
-                style={{
-                  flex: 0,
-                  width: 110,
-                  borderRadius: 5,
-                  backgroundColor: '#5FCF1B',
-                  height: 35,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text
-                  style={{
-                    color: '#F7FF9C',
-                    fontSize: 16,
-                    fontWeight: '500',
-                  }}>
-                  Save
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </ScrollView>
-        </View>
-      </Modal>
+      <SaveKid
+        isModalVisible={isModalVisible}
+        setModalVisible={setModalVisible}
+        toggleModal={toggleModal}
+      />
     </>
   );
 };

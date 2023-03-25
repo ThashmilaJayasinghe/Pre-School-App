57230df73c2831e20ebcd2fe0dcb78a058d764bd
Ramath <ramathmanjitha@gmail.com>
2023-03-25 11:09:05 +0530

    add kid

--- src/components/studentRoster/SaveKid.js
+++ src/components/studentRoster/SaveKid.js
@@ -1,7 +1,6 @@
 import {
   View,
   Text,
-  Button,
   TouchableOpacity,
   TextInput,
   ScrollView,
@@ -9,16 +8,16 @@ import {
 import React, {useState} from 'react';
 import Modal from 'react-native-modal';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import {FontFamily} from '../../GlobalStyles';
-import Textarea from 'react-native-textarea';
+import KidProfile from './KidProfile';
 
-const SaveKid = props => {
-  const [comment, setComment] = useState('');
-  const [rating, setRating] = useState(0.0);
+const SaveKid = ({isModalVisible, setModalVisible, toggleModal}) => {
+  const [kidId, setKidId] = useState('');
+  const [name, setName] = useState('');
+  const [kidClass, setKidClass] = useState('');
 
   return (
     <View style={{flex: 1}}>
-      <Button title="Show modal" onPress={props.toggleModal} />
+      {/* //Modal */}
 
       <Modal
         isVisible={isModalVisible}
@@ -33,64 +32,113 @@ const SaveKid = props => {
             borderRadius: 20,
             paddingBottom: 20,
           }}>
+          <TouchableOpacity
+            style={{marginTop: 13, marginRight: 13, alignItems: 'flex-end'}}
+            onPress={toggleModal}>
+            <AntDesign name="closesquare" size={27} color="#F47B0B" />
+          </TouchableOpacity>
+
+          <View style={{alignItems: 'center', marginVertical: 10}}>
+            <Text style={{fontSize: 17, fontWeight: 500}}>Add New Kid</Text>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              marginBottom: 5,
+            }}>
+            <KidProfile size={100} />
+          </View>
+
           <ScrollView>
-            <TouchableOpacity
-              style={{marginTop: 13, marginRight: 13, alignItems: 'flex-end'}}
-              onPress={toggleModal}>
-              <AntDesign name="closesquare" size={27} color="#F47B0B" />
-            </TouchableOpacity>
-
-            <View style={{alignItems: 'center', marginVertical: 10}}>
-              <Text style={{fontSize: 17, fontWeight: 500}}>Add Feedback</Text>
-            </View>
-
-            <View
-              style={{
-                marginTop: 20,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View style={{flexDirection: 'row', paddingTop: 5}}>
-                <Text
-                  style={{
-                    color: '#0E1979',
-                    fontFamily: FontFamily.alefRegular,
-                  }}>
-                  Student Name
-                </Text>
-                <Text style={{color: '#545FC3'}}> : Lahiru</Text>
-              </View>
-              <View style={{flexDirection: 'row', paddingTop: 5}}>
-                <Text style={{color: '#0E1979'}}>Student ID</Text>
-                <Text style={{color: '#545FC3'}}> : L00342</Text>
-              </View>
-              <View style={{flexDirection: 'row', paddingTop: 5}}>
-                <Text style={{color: '#0E1979'}}>Student Class</Text>
-                <Text style={{color: '#545FC3'}}> : Class A</Text>
-              </View>
-            </View>
             <View style={{margin: 20, maxHeight: 300}}>
-              <Text style={{marginBottom: 10}}>Comment</Text>
+              <Text
+                style={{
+                  fontFamily: 'Alatsi',
+                  fontWeight: 400,
+                  fontSize: 14,
+                  lineHeight: 15,
+                  marginBottom: 10,
+                  color: '#616161',
+                }}>
+                Kid's ID
+              </Text>
 
-              <Textarea
-                containerStyle={{
-                  height: '70%',
-                  padding: 5,
-                  backgroundColor: '#e8e9eb',
+              <TextInput
+                onChangeText={setKidId}
+                style={{
                   borderWidth: 1,
                   borderColor: '#D6D6D6',
                   borderRadius: 10,
                   opacity: 10,
-                }}
-                style={{
-                  textAlignVertical: 'top',
-                  height: 170,
+                  padding: 5,
+                  backgroundColor: '#e8e9eb',
                   fontSize: 14,
                   color: '#8d8d8f',
                 }}
-                onChangeText={setComment}
-                maxLength={120}
-                placeholder={'Comment here ...'}
+                placeholder={`Add kid's ID here ...`}
+                placeholderTextColor={'#a7a7a8'}
+                underlineColorAndroid={'transparent'}
+              />
+            </View>
+
+            <View style={{margin: 20, maxHeight: 300}}>
+              <Text
+                style={{
+                  fontFamily: 'Alatsi',
+                  fontWeight: 400,
+                  fontSize: 14,
+                  lineHeight: 15,
+                  marginBottom: 10,
+                  color: '#616161',
+                }}>
+                Name
+              </Text>
+
+              <TextInput
+                onChangeText={setName}
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#D6D6D6',
+                  borderRadius: 10,
+                  opacity: 10,
+                  padding: 5,
+                  backgroundColor: '#e8e9eb',
+                  fontSize: 14,
+                  color: '#8d8d8f',
+                }}
+                placeholder={`Add kid's name here ...`}
+                placeholderTextColor={'#a7a7a8'}
+                underlineColorAndroid={'transparent'}
+              />
+            </View>
+            <View style={{margin: 20, maxHeight: 300}}>
+              <Text
+                style={{
+                  fontFamily: 'Alatsi',
+                  fontWeight: 400,
+                  fontSize: 14,
+                  lineHeight: 15,
+                  marginBottom: 10,
+                  color: '#616161',
+                }}>
+                Class
+              </Text>
+
+              <TextInput
+                onChangeText={setKidClass}
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#D6D6D6',
+                  borderRadius: 10,
+                  opacity: 10,
+                  padding: 5,
+                  backgroundColor: '#e8e9eb',
+                  fontSize: 14,
+                  color: '#8d8d8f',
+                }}
+                placeholder={`Add kid's class here ...`}
                 placeholderTextColor={'#a7a7a8'}
                 underlineColorAndroid={'transparent'}
               />
@@ -98,6 +146,7 @@ const SaveKid = props => {
 
             <View style={{alignItems: 'center', marginBottom: 20}}>
               <TouchableOpacity
+                // onPress={addKid}
                 style={{
                   flex: 0,
                   width: 110,
@@ -113,7 +162,7 @@ const SaveKid = props => {
                     fontSize: 16,
                     fontWeight: '500',
                   }}>
-                  Submit
+                  Save
                 </Text>
               </TouchableOpacity>
             </View>
